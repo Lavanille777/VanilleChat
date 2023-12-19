1bac686cc6e802fe7b3c00c6a70e4aa592cdeeb6
tangxingyu <lavanille777@gmail.com>
2023-12-19 22:01:44 +0800

    Initial Commit

---
+++ VanilleChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VanilleChat
+//
+//  Created by lavanille on 2023/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
